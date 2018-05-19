a921e4c9f094d46e1e5cc42bf1319ec638abba3a
radoslawSz <radoslaw.szpionkowski@gmail.com>
2018-05-19 12:54:27 +0200

    example2

--- src/test/java/com/example/fruitshop/controller/CategoryControllerTest.java
+++ src/test/java/com/example/fruitshop/controller/CategoryControllerTest.java
@@ -14,11 +14,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
